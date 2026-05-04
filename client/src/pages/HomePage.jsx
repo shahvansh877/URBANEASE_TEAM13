ad025228479b4ccb2355efab22e1a510149eb1b5
Pragadeeshwar Vijay <dvpppragadeeshwar@gmail.com>
2026-05-04 11:16:35 +0530

    chatbot fixing issue live

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -73,7 +73,7 @@ function extractChatbotReply(data) {
 
 async function askUrbanBot(message) {
   try {
-    const res = await fetch(`${API}/chatbot`, {
+    const res = await fetch(`${API}/chat`, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ message }),
